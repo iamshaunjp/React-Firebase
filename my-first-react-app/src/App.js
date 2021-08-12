2232fa7a266e590dc4fe5b7cc4882ee4565c9783
Shaun <shaun@thenetninja.co.uk>
2021-08-12 13:58:23 +0100

    lesson-14

--- my-first-react-app/src/App.js
+++ my-first-react-app/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React and Firebase
         </a>
       </header>
     </div>
