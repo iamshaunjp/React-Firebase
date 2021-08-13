cc39183c2648dc8136d60ceae41f97359055bc8d
Shaun <shaun@thenetninja.co.uk>
2021-08-13 13:36:36 +0100

    lesson-20

--- my-first-react-app/src/App.js
+++ my-first-react-app/src/App.js
@@ -3,6 +3,11 @@ import { useState } from 'react'
 
 function App() {
   const [name, setName] = useState('mario')
+  const [events, setEvents] = useState([
+    {title: "mario's birthday bash", id: 1},
+    {title: "bowser's live stream", id: 2},
+    {title: "race on moo moo farm", id: 3}
+  ])
 
   const handleClick = () => {
     setName('luigi')
@@ -13,6 +18,11 @@ function App() {
     <div className="App">
       <h1>My name is {name}</h1>
       <button onClick={handleClick}>Change name</button>
+      {events.map((event, index) => (
+        <div key={event.id}>
+          <h2>{index} - {event.title}</h2>
+        </div>
+      ))}
     </div>
   );
 }
