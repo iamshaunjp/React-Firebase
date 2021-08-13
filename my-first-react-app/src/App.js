812c6aa6616b1b5d491a2c90b2e17b06ec3f78fc
Shaun <shaun@thenetninja.co.uk>
2021-08-13 13:12:24 +0100

    lesson-17

--- my-first-react-app/src/App.js
+++ my-first-react-app/src/App.js
@@ -2,22 +2,17 @@
 import './App.css';
 
 function App() {
+  let name = 'mario'
+
+  const handleClick = () => {
+    name = 'luigi'
+    console.log(name)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src='/logo.svg' className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React and Firebase
-        </a>
-      </header>
+      <h1>My name is {name}</h1>
+      <button onClick={handleClick}>Change name</button>
     </div>
   );
 }
