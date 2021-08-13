d9f224f4e4b9488f4cb9e9dd87852a8e3621381b
Shaun <shaun@thenetninja.co.uk>
2021-08-13 13:46:04 +0100

    lesson-22

--- my-first-react-app/src/App.js
+++ my-first-react-app/src/App.js
@@ -2,11 +2,14 @@ import './App.css'
 import { useState } from 'react'
 
 function App() {
+  const [showEvents, setShowEvents] = useState(true)
   const [events, setEvents] = useState([
     {title: "mario's birthday bash", id: 1},
     {title: "bowser's live stream", id: 2},
     {title: "race on moo moo farm", id: 3}
   ])
+
+  console.log(showEvents)
 
   const handleClick = (id) => {
     setEvents(prevEvents => {
@@ -16,7 +19,14 @@ function App() {
 
   return (
     <div className="App">
-      {events.map((event, index) => (
+      {showEvents && (
+        <div>
+          <button onClick={() => setShowEvents(false)}>Hide Events</button>
+        </div>
+      )}
+      {!showEvents && <button onClick={() => setShowEvents(true)}>Show Events</button>}
+      
+      {showEvents && events.map((event, index) => (
         <div key={event.id}>
           <h2>{index} - {event.title}</h2>
           <button onClick={() => handleClick(event.id)}>delete event</button>
