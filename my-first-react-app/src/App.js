39437eaa2c8cade6e433e045781643b9382efca7
Shaun <shaun@thenetninja.co.uk>
2021-08-15 17:05:25 +0100

    lesson-25

--- my-first-react-app/src/App.js
+++ my-first-react-app/src/App.js
@@ -25,8 +25,11 @@ function App() {
           <button onClick={() => setShowEvents(false)}>Hide Events</button>
         </div>
       )}
-      {!showEvents && <button onClick={() => setShowEvents(true)}>Show Events</button>}
-      
+      {!showEvents && (
+        <div>
+          <button onClick={() => setShowEvents(true)}>Show Events</button>
+        </div>
+      )}
       {showEvents && events.map((event, index) => (
         <div key={event.id}>
           <h2>{index} - {event.title}</h2>
