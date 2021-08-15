c368af91e1191b2f40101de3cbe5121eaf945666
Shaun <shaun@thenetninja.co.uk>
2021-08-15 17:09:24 +0100

    lesson-31

--- my-first-react-app/src/components/Modal.js
+++ my-first-react-app/src/components/Modal.js
@@ -1,12 +1,13 @@
+import ReactDOM from 'react-dom'
 import './Modal.css'
 
 export default function Modal({ children, handleClose }) {
-  return (
+  return ReactDOM.createPortal((
     <div className="modal-backdrop">
       <div className="modal">
         {children}
         <button onClick={handleClose}>Close</button>
       </div>
     </div>
-  )
+  ), document.body)
 }
