86f295fb32e526c96772dca773e91a60c78e3de9
Shaun <shaun@thenetninja.co.uk>
2021-08-16 12:09:20 +0100

    lesson-39

--- my-first-react-app/src/components/EventList.js
+++ my-first-react-app/src/components/EventList.js
@@ -1,13 +1,14 @@
 import React from 'react'
+import styles from './EventList.module.css'
 
 export default function EventList({ events, handleClick }) {
   return (
     <div>
       {events.map((event, index) => (
-        <React.Fragment key={event.id}>
+        <div className={styles.card} key={event.id}>
           <h2>{index} - {event.title}</h2>
           <button onClick={() => handleClick(event.id)}>delete event</button>
-        </React.Fragment>
+        </div>
       ))}
     </div>
   )
