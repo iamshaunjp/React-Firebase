c3d641ae82ad489d9a632c5d44b964ed02a00b8e
Shaun <shaun@thenetninja.co.uk>
2021-08-16 15:53:10 +0100

    lesson-41

--- my-first-react-app/src/components/NewEventForm.js
+++ my-first-react-app/src/components/NewEventForm.js
@@ -1,19 +1,26 @@
+import { useState } from 'react'
 import './NewEventForm.css'
 
 export default function NewEventForm() {
+  const [title, setTitle] = useState(null)
+  const [date, setDate] = useState(null)
+
+  // const handleChange = (e) => {
+  //   console.log(e.target.value)
+  // }
+
   return (
     <form className="new-event-form">
-      {/* <label htmlFor="title">Event Title:</label>
-      <input type="text" id="title" /> */}
       <label>
         <span>Event Title:</span>
-        <input type="text" />
+        <input type="text" onChange={(e) => setTitle(e.target.value)} />
       </label>
       <label>
         <span>Event Date:</span>
-        <input type="date" />
+        <input type="date" onChange={(e) => setDate(e.target.value)} />
       </label>
       <button>Submit</button>
+      <p>title - {title}, date - {date}</p>
     </form>
   )
 }
