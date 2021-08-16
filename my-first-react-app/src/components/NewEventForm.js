c66dbf305f548d25170bb9fedbeffca53a7c6498
Shaun <shaun@thenetninja.co.uk>
2021-08-16 16:27:08 +0100

    lesson-43

--- my-first-react-app/src/components/NewEventForm.js
+++ my-first-react-app/src/components/NewEventForm.js
@@ -10,27 +10,36 @@ export default function NewEventForm() {
     setDate('')
   }
 
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const event = {
+      title: title,
+      date: date,
+      id: Math.floor(Math.random() * 10000)
+    }
+    console.log(event)
+    resetForm()
+  }
+
   return (
-    <form className="new-event-form">
+    <form className="new-event-form" onSubmit={handleSubmit}>
       <label>
         <span>Event Title:</span>
-        <input 
-          type="text" 
+        <input
+          type="text"
           onChange={(e) => setTitle(e.target.value)}
           value={title}
         />
       </label>
       <label>
         <span>Event Date:</span>
-        <input 
-          type="date" 
-          onChange={(e) => setDate(e.target.value)} 
+        <input
+          type="date"
+          onChange={(e) => setDate(e.target.value)}
           value={date}
         />
       </label>
       <button>Submit</button>
-      <p>title - {title}, date - {date}</p>
-      <p onClick={resetForm}>reset the form</p>
     </form>
   )
 }
