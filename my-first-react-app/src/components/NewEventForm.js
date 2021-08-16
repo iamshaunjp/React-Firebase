39ce0356959e275e976740b0b1049546101d1ede
Shaun <shaun@thenetninja.co.uk>
2021-08-16 16:21:58 +0100

    lesson-42

--- my-first-react-app/src/components/NewEventForm.js
+++ my-first-react-app/src/components/NewEventForm.js
@@ -2,25 +2,35 @@ import { useState } from 'react'
 import './NewEventForm.css'
 
 export default function NewEventForm() {
-  const [title, setTitle] = useState(null)
-  const [date, setDate] = useState(null)
+  const [title, setTitle] = useState('')
+  const [date, setDate] = useState('')
 
-  // const handleChange = (e) => {
-  //   console.log(e.target.value)
-  // }
+  const resetForm = () => {
+    setTitle('')
+    setDate('')
+  }
 
   return (
     <form className="new-event-form">
       <label>
         <span>Event Title:</span>
-        <input type="text" onChange={(e) => setTitle(e.target.value)} />
+        <input 
+          type="text" 
+          onChange={(e) => setTitle(e.target.value)}
+          value={title}
+        />
       </label>
       <label>
         <span>Event Date:</span>
-        <input type="date" onChange={(e) => setDate(e.target.value)} />
+        <input 
+          type="date" 
+          onChange={(e) => setDate(e.target.value)} 
+          value={date}
+        />
       </label>
       <button>Submit</button>
       <p>title - {title}, date - {date}</p>
+      <p onClick={resetForm}>reset the form</p>
     </form>
   )
 }
