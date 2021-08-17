623f3897e0558693b563f1541ab6dbea56ec6ec1
Shaun <shaun@thenetninja.co.uk>
2021-08-17 17:32:58 +0100

    lesson-49

--- tripago/src/components/TripList.js
+++ tripago/src/components/TripList.js
@@ -1,17 +1,28 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import './TripList.css'
 
 export default function TripList() {
   const [trips, setTrips] = useState([])
 
   console.log(trips)
 
-  fetch('http://localhost:3000/trips')
-    .then(response => response.json())
-    .then(json => setTrips(json))
+  useEffect(() => {
+    fetch('http://localhost:3000/trips')
+      .then(response => response.json())
+      .then(json => setTrips(json))
+  }, [])
 
   return (
-    <div>
+    <div className="trip-list">
       <h2>Trip List</h2>
+      <ul>
+        {trips.map(trip => (
+          <li key={trip.id}>
+            <h3>{trip.title}</h3>
+            <p>{trip.price}</p>
+          </li>
+        ))}
+      </ul>
     </div>
   )
 }
