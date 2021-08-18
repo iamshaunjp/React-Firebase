c62026eb900e006c2a5e5b1636de7643c8267ab8
Shaun <shaun@thenetninja.co.uk>
2021-08-18 10:52:17 +0100

    lesson-50

--- tripago/src/components/TripList.js
+++ tripago/src/components/TripList.js
@@ -3,14 +3,14 @@ import './TripList.css'
 
 export default function TripList() {
   const [trips, setTrips] = useState([])
-
-  console.log(trips)
+  const [url, setUrl] = useState('http://localhost:3000/trips')
 
   useEffect(() => {
-    fetch('http://localhost:3000/trips')
+    console.log('useEffect function ran')
+    fetch(url)
       .then(response => response.json())
       .then(json => setTrips(json))
-  }, [])
+  }, [url])
 
   return (
     <div className="trip-list">
@@ -23,6 +23,14 @@ export default function TripList() {
           </li>
         ))}
       </ul>
+      <div className="filters">
+        <button onClick={() => setUrl('http://localhost:3000/trips?loc=europe')}>
+          European Trips
+        </button>
+        <button onClick={() => setUrl('http://localhost:3000/trips')}>
+          All Trips
+        </button>
+      </div>
     </div>
   )
 }
