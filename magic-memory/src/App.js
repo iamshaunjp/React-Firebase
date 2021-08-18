686468081cb741c30f851f875f016c0e8371e83e
Shaun <shaun@thenetninja.co.uk>
2021-08-18 18:16:09 +0100

    lesson-64

--- magic-memory/src/App.js
+++ magic-memory/src/App.js
@@ -3,12 +3,12 @@ import './App.css'
 import SingleCard from './components/SingleCard'
 
 const cardImages = [
-  { "src": "/img/helmet-1.png" },
-  { "src": "/img/potion-1.png" },
-  { "src": "/img/ring-1.png" },
-  { "src": "/img/scroll-1.png" },
-  { "src": "/img/shield-1.png" },
-  { "src": "/img/sword-1.png" },
+  { "src": "/img/helmet-1.png", matched: false },
+  { "src": "/img/potion-1.png", matched: false },
+  { "src": "/img/ring-1.png", matched: false },
+  { "src": "/img/scroll-1.png", matched: false },
+  { "src": "/img/shield-1.png", matched: false },
+  { "src": "/img/sword-1.png", matched: false },
 ]
 
 function App() {
@@ -39,6 +39,15 @@ function App() {
 
       if (choiceOne.src === choiceTwo.src) {
         console.log('those cards match')
+        setCards(prevCards => {
+          return prevCards.map(card => {
+            if (card.src === choiceOne.src) {
+              return { ...card, matched: true }
+            } else {
+              return card
+            }
+          })
+        })
         resetTurn()
       } else {
         console.log('those cards do not match')
@@ -47,6 +56,8 @@ function App() {
 
     }
   }, [choiceOne, choiceTwo])
+
+  console.log(cards)
 
   // reset choices & increase turn
   const resetTurn = () => {
