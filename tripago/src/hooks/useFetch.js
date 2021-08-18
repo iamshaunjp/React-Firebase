de5876f73e550904cc8de120701cf2f6a4ea1567
Shaun <shaun@thenetninja.co.uk>
2021-08-18 12:59:46 +0100

    lesson-52

--- tripago/src/hooks/useFetch.js
+++ tripago/src/hooks/useFetch.js
@@ -4,8 +4,8 @@ export const useFetch = (url) => {
   const [data, setData] = useState(null)
 
   useEffect(() => {
-    const fetchData = async (fetchOptions) => {
-      const res = await fetch(url, fetchOptions)
+    const fetchData = async () => {
+      const res = await fetch(url)
       const data = await res.json()
 
       setData(data)
