a19538d9f70fa145aa8e11cc494a5d6b244b364d
Shaun <shaun@thenetninja.co.uk>
2021-08-18 18:03:39 +0100

    lesson-62

--- magic-memory/src/App.js
+++ magic-memory/src/App.js
@@ -14,6 +14,8 @@ const cardImages = [
 function App() {
   const [cards, setCards] = useState([])
   const [turns, setTurns] = useState(0)
+  const [choiceOne, setChoiceOne] = useState(null)
+  const [choiceTwo, setChoiceTwo] = useState(null)
 
   // shuffle cards for new game
   const shuffleCards = () => {
@@ -25,7 +27,11 @@ function App() {
     setTurns(0)
   }
 
-  console.log(cards, turns)
+  // handle a choice
+  const handleChoice = (card) => {
+    console.log(card)
+    choiceOne ? setChoiceTwo(card) : setChoiceOne(card)
+  }
 
   return (
     <div className="App">
@@ -37,6 +43,7 @@ function App() {
           <SingleCard 
             key={card.id}
             card={card}
+            handleChoice={handleChoice}
           />
         ))}
       </div>
