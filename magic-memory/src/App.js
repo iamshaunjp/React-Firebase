fc65e4571a3966895dd12495746068fe90862104
Shaun <shaun@thenetninja.co.uk>
2021-08-18 17:37:11 +0100

    lesson-59

--- magic-memory/src/App.js
+++ magic-memory/src/App.js
@@ -1,9 +1,35 @@
+import { useState } from 'react'
 import './App.css'
 
+const cardImages = [
+  { "src": "/img/helmet-1.png" },
+  { "src": "/img/potion-3.png" },
+  { "src": "/img/potion-4.png" },
+  { "src": "/img/scroll-1.png" },
+  { "src": "/img/shield-1.png" },
+  { "src": "/img/ring-1.png" },
+]
+
 function App() {
+  const [cards, setCards] = useState([])
+  const [turns, setTurns] = useState(0)
+
+  // shuffle cards for new game
+  const shuffleCards = () => {
+    const cards = [...cardImages, ...cardImages]
+      .sort(() => Math.random() - 0.5)
+      .map(card => ({ ...card, id: Math.random() }))
+      
+    setCards(cards)
+    setTurns(0)
+  }
+
+  console.log(cards, turns)
+
   return (
     <div className="App">
-      
+      <h1>Magic Match</h1>
+      <button onClick={shuffleCards}>New Game</button>
     </div>
   );
 }
