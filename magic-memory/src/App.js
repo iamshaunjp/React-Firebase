d7a05986da915d01a835ae239f19bb39ecc410da
Shaun <shaun@thenetninja.co.uk>
2021-08-19 14:23:16 +0100

    test

--- magic-memory/src/App.js
+++ magic-memory/src/App.js
@@ -80,7 +80,7 @@ function App() {
             key={card.id}
             card={card}
             handleChoice={handleChoice}
-            flipped={card === choiceOne || card === choiceTwo}
+            flipped={card === choiceOne || card === choiceTwo || card.matched}
             disabled={disabled}
           />
         ))}
