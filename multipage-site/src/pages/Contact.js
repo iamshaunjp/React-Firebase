d9695da58517cf83b9f5d2245a64bcc05338c5c0
Shaun <shaun@thenetninja.co.uk>
2021-08-19 16:44:30 +0100

    lesson-78

--- multipage-site/src/pages/Contact.js
+++ multipage-site/src/pages/Contact.js
@@ -1,7 +1,15 @@
+import { useLocation } from "react-router-dom"
+
 export default function Contact() {
+  const queryString = useLocation().search
+  // console.log(queryString)
+  
+  const queryParams = new URLSearchParams(queryString)
+  const name = queryParams.get("name")
+  
   return (
     <div>
-      <h2>Contact</h2>
+      <h2>Hey {name}, Contact Us</h2>
       <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsam explicabo laudantium nemo voluptas cum omnis error voluptate. Nihil numquam ipsum necessitatibus hic odit neque consequuntur dolor. Magni quos ratione iste.</p>
     </div>
   )
