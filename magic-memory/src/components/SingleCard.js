0fbabcf149ff3a1efd159573fc3813a1e742996c
Shaun <9838872+iamshaunjp@users.noreply.github.com>
2021-08-19 13:46:47 +0100

    Update SingleCard.js

--- magic-memory/src/components/SingleCard.js
+++ magic-memory/src/components/SingleCard.js
@@ -2,8 +2,6 @@ import './SingleCard.css'
 
 export default function SingleCard({ card, handleChoice, flipped, disabled }) {
 
-  const isFlipped = flipped || card.matched
-
   const handleClick = () => {
     if (!disabled) {
       handleChoice(card)
@@ -12,7 +10,7 @@ export default function SingleCard({ card, handleChoice, flipped, disabled }) {
 
   return (
     <div className="card">
-      <div className={isFlipped ? "flipped" : ""}>
+      <div className={flipped ? "flipped" : ""}>
         <img className="front" src={card.src} alt="card front" />
         <img className="back" src="/img/cover.png" onClick={handleClick} alt="cover" />
       </div>
