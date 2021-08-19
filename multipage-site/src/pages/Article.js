fd94d89d06d1c69f5c1443223b1e876e03831178
Shaun <shaun@thenetninja.co.uk>
2021-08-19 16:26:26 +0100

    lesson-75

--- multipage-site/src/pages/Article.js
+++ multipage-site/src/pages/Article.js
@@ -1,7 +1,23 @@
+import { useParams } from "react-router-dom"
+import { useFetch } from "../hooks/useFetch"
+
 export default function Article() {
+  // const params = useParams()
+  const { id } = useParams()
+  const url = 'http://localhost:3000/articles/' + id
+  const { data: article, isPending, error } = useFetch(url)
+
   return (
     <div>
-      <h2>Article Details</h2>
+      {isPending && <p>Loading...</p>}
+      {error && <p>{error}</p>}
+      {article && (
+        <div key={article.id}>
+          <h2>{article.title}</h2>
+          <p>By {article.author}</p>
+          <p>{article.body}</p>
+        </div>
+      )}
     </div>
   )
 }
