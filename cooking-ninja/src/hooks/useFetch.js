c5609581c94deff8689d74f2769e12cfa55998d3
Shaun <shaun@thenetninja.co.uk>
2021-08-20 11:46:24 +0100

    lesson-88

--- cooking-ninja/src/hooks/useFetch.js
+++ cooking-ninja/src/hooks/useFetch.js
@@ -1,18 +1,29 @@
 import { useState, useEffect } from "react"
 
-export const useFetch = (url) => {
+export const useFetch = (url, method = "GET") => {
   const [data, setData] = useState(null)
   const [isPending, setIsPending] = useState(false)
   const [error, setError] = useState(null)
+  const [options, setOptions] = useState(null)
+
+  const postData = (postData) => {
+    setOptions({
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(postData)
+    })
+  }
 
   useEffect(() => {
     const controller = new AbortController()
 
-    const fetchData = async () => {
+    const fetchData = async (fetchOptions) => {
       setIsPending(true)
       
       try {
-        const res = await fetch(url, { signal: controller.signal })
+        const res = await fetch(url, { ...fetchOptions, signal: controller.signal })
         if(!res.ok) {
           throw new Error(res.statusText)
         }
@@ -31,13 +42,19 @@ export const useFetch = (url) => {
       }
     }
 
-    fetchData()
+    // invoke the function
+    if (method === "GET") {
+      fetchData()
+    }
+    if (method === "POST" && options) {
+      fetchData(options)
+    }
 
     return () => {
       controller.abort()
     }
 
-  }, [url])
+  }, [url, method, options])
 
-  return { data, isPending, error }
+  return { data, isPending, error, postData }
 }
--- cooking-ninja/src/pages/create/Create.js
+++ cooking-ninja/src/pages/create/Create.js
@@ -1,4 +1,5 @@
 import { useState, useRef } from 'react'
+import { useFetch } from '../../hooks/useFetch'
 
 // styles
 import './Create.css'
@@ -10,10 +11,12 @@ export default function Create() {
   const [newIngredient, setNewIngredient] = useState('')
   const [ingredients, setIngredients] = useState([])
   const ingredientInput = useRef(null)
+
+  const { postData, data, error } = useFetch('http://localhost:3000/recipes', 'POST')
   
   const handleSubmit = (e) => {
     e.preventDefault()
-    console.log(title, method, cookingTime, ingredients)
+    postData({ title, ingredients, method, cookingTime: cookingTime + ' minutes' })
   }
 
   const handleAdd = (e) => {
