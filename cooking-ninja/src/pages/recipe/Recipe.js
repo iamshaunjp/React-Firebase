2388f18463caac48c29fcdc5cc4b63bac18e1d43
Shaun <shaun@thenetninja.co.uk>
2021-08-20 11:23:00 +0100

    lesson-84

--- cooking-ninja/src/pages/recipe/Recipe.js
+++ cooking-ninja/src/pages/recipe/Recipe.js
@@ -1,10 +1,19 @@
+import { useParams } from 'react-router-dom'
+import { useFetch } from '../../hooks/useFetch'
+
 // styles
 import './Recipe.css'
 
 export default function Recipe() {
+  const { id } = useParams()
+  const url = 'http://localhost:3000/recipes/' + id
+  const { error, isPending, data: recipe } = useFetch(url)
+
   return (
-    <div>
-      Recipe
+    <div className="recipe">
+      {error && <p className="error">{error}</p>}
+      {isPending && <p className="loading">Loading...</p>}
+      {recipe && <h1>{recipe.title}</h1>}
     </div>
   )
 }
