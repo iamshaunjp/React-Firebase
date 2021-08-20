b11390c239354e54b0774d13ba1faba487a270fb
Shaun <shaun@thenetninja.co.uk>
2021-08-20 11:09:09 +0100

    lesson-82

---
+++ cooking-ninja/src/hooks/useFetch.js
@@ -0,0 +1,43 @@
+import { useState, useEffect } from "react"
+
+export const useFetch = (url) => {
+  const [data, setData] = useState(null)
+  const [isPending, setIsPending] = useState(false)
+  const [error, setError] = useState(null)
+
+  useEffect(() => {
+    const controller = new AbortController()
+
+    const fetchData = async () => {
+      setIsPending(true)
+      
+      try {
+        const res = await fetch(url, { signal: controller.signal })
+        if(!res.ok) {
+          throw new Error(res.statusText)
+        }
+        const data = await res.json()
+
+        setIsPending(false)
+        setData(data)
+        setError(null)
+      } catch (err) {
+        if (err.name === "AbortError") {
+          console.log("the fetch was aborted")
+        } else {
+          setIsPending(false)
+          setError('Could not fetch the data')
+        }
+      }
+    }
+
+    fetchData()
+
+    return () => {
+      controller.abort()
+    }
+
+  }, [url])
+
+  return { data, isPending, error }
+}
--- cooking-ninja/src/pages/home/Home.js
+++ cooking-ninja/src/pages/home/Home.js
@@ -1,10 +1,18 @@
+import { useFetch } from '../../hooks/useFetch'
+
 // styles
 import './Home.css'
 
 export default function Home() {
+  const { data, isPending, error } = useFetch('http://localhost:3000/recipes')
+
   return (
-    <div>
-      Home
+    <div className="home">
+      {error && <p className="error">{error}</p>}
+      {isPending && <p className="loading">Loading...</p>}
+      {data && data.map(recipe => (
+        <h2 key={recipe.id}>{recipe.title}</h2>
+      ))}
     </div>
   )
 }
