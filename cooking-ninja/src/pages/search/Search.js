5d022ef18e0be7cebf602099930a0743f1c52c50
Shaun <shaun@thenetninja.co.uk>
2021-08-20 12:37:43 +0100

    lesson-91

--- cooking-ninja/src/pages/search/Search.js
+++ cooking-ninja/src/pages/search/Search.js
@@ -1,10 +1,24 @@
+import { useFetch } from '../../hooks/useFetch'
+import { useLocation } from 'react-router-dom'
+import RecipeList from '../../components/RecipeList'
+
 // styles
 import './Search.css'
 
 export default function Search() {
+  const queryString = useLocation().search
+  const queryParams = new URLSearchParams(queryString)
+  const query = queryParams.get('q')
+
+  const url = 'http://localhost:3000/recipes?q=' + query
+  const { error, isPending, data } = useFetch(url)
+
   return (
     <div>
-      Search
+      <h2 className="page-title">Recipes including "{query}"</h2>
+      {error && <p className="error">{error}</p>}
+      {isPending && <p className="loading">Loading...</p>}
+      {data && <RecipeList recipes={data} />}
     </div>
   )
 }
