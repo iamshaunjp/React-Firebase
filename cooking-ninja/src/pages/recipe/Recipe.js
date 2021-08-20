f366e0f60904f4ce3ae81e203eef70ef2f702440
Shaun <shaun@thenetninja.co.uk>
2021-08-20 11:25:04 +0100

    lesson-85

--- cooking-ninja/src/pages/recipe/Recipe.js
+++ cooking-ninja/src/pages/recipe/Recipe.js
@@ -13,7 +13,16 @@ export default function Recipe() {
     <div className="recipe">
       {error && <p className="error">{error}</p>}
       {isPending && <p className="loading">Loading...</p>}
-      {recipe && <h1>{recipe.title}</h1>}
+      {recipe && (
+        <>
+          <h2 className="page-title">{recipe.title}</h2>
+          <p>Takes {recipe.cookingTime} to cook.</p>
+          <ul>
+            {recipe.ingredients.map(ing => <li key={ing}>ing</li>)}
+          </ul>
+          <p className="method">{recipe.method}</p>
+        </>
+      )}
     </div>
   )
 }
