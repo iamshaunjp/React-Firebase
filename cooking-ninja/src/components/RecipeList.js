696c4f414752f9ea41410845f4a6bba806a52d89
Shaun <shaun@thenetninja.co.uk>
2021-08-20 12:42:14 +0100

    lesson-92

--- cooking-ninja/src/components/RecipeList.js
+++ cooking-ninja/src/components/RecipeList.js
@@ -4,6 +4,11 @@ import { Link } from 'react-router-dom'
 import './RecipeList.css'
 
 export default function RecipeList({ recipes }) {
+
+  if (recipes.length === 0) {
+    return <div className="error">No recipes to load...</div>
+  }
+
   return (
     <div className="recipe-list">
       {recipes.map(recipe => (
