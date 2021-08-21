d1416fde89b797e3a0a3a876e0aeb9d03b48f0fd
Shaun <shaun@thenetninja.co.uk>
2021-08-21 16:52:35 +0100

    lesson-98

--- cooking-ninja/src/context/ThemeContext.js
+++ cooking-ninja/src/context/ThemeContext.js
@@ -1,11 +1,25 @@
-import { createContext } from 'react'
+import { createContext, useReducer } from 'react'
 
 export const ThemeContext = createContext()
 
+const themeReducer = (state, action) => {
+  switch (action.type) {
+    case 'CHANGE_COLOR':
+      return { ...state, color: action.payload }
+  }
+}
+
 export function ThemeProvider({ children }) {
+  const [state, dispatch] = useReducer(themeReducer, {
+    color: 'blue'
+  })
+
+  const changeColor = (color) => {
+    dispatch({ type: 'CHANGE_COLOR', payload: color })
+  }
 
   return (
-    <ThemeContext.Provider value={{ color: 'blue' }}>
+    <ThemeContext.Provider value={{...state, changeColor}}>
       {children}
     </ThemeContext.Provider>
   )
