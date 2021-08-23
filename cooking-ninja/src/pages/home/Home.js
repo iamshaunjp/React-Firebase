20252e39de73bb7eca48a1c92882fb436d48916c
Shaun <shaun@thenetninja.co.uk>
2021-08-23 15:46:06 +0100

    lesson-105

--- cooking-ninja/src/pages/home/Home.js
+++ cooking-ninja/src/pages/home/Home.js
@@ -1,11 +1,37 @@
-import { useFetch } from '../../hooks/useFetch'
+import { useState, useEffect } from 'react'
+import { projectFirestore } from '../../firebase/config'
 import RecipeList from '../../components/RecipeList'
 
 // styles
 import './Home.css'
 
 export default function Home() {
-  const { data, isPending, error } = useFetch('http://localhost:3000/recipes')
+  const [data, setData] = useState(null)
+  const [isPending, setIsPending] = useState(false)
+  const [error, setError] = useState(false)
+
+  useEffect(() => {
+    setIsPending(true)
+
+    projectFirestore.collection('recipes').get().then(snapshot => {
+      if (snapshot.empty) {
+        setError('No recipes to load')
+        setIsPending(false)
+      } else {
+        let results = []
+        snapshot.docs.forEach(doc => {
+          // console.log(doc)
+          results.push({ ...doc.data(), id: doc.id })
+        })
+        setData(results)
+        setIsPending(false)
+      }
+    }).catch(err => {
+      setError(err.message)
+      setIsPending(false)
+    })
+
+  }, [])
 
   return (
     <div className="home">
