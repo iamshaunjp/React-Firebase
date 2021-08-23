cd3cea189175f0f77bf6858e40ffe90c397e50a7
Shaun <shaun@thenetninja.co.uk>
2021-08-23 16:13:38 +0100

    lesson-110

--- cooking-ninja/src/pages/recipe/Recipe.js
+++ cooking-ninja/src/pages/recipe/Recipe.js
@@ -29,6 +29,12 @@ export default function Recipe() {
 
   }, [id])
 
+  const handleClick = () => {
+    projectFirestore.collection('recipes').doc(id).update({
+      title: 'Something completely different'
+    })
+  }
+
   return (
     <div className={`recipe ${mode}`}>
       {error && <p className="error">{error}</p>}
@@ -41,6 +47,7 @@ export default function Recipe() {
             {recipe.ingredients.map(ing => <li key={ing}>ing</li>)}
           </ul>
           <p className="method">{recipe.method}</p>
+          <button onClick={handleClick}>Update me</button>
         </>
       )}
     </div>
