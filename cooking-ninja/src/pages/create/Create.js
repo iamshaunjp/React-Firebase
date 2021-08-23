4d9e2f0aa12508b509965412475ed0f3a63fcedd
Shaun <shaun@thenetninja.co.uk>
2021-08-23 16:01:39 +0100

    lesson-107

--- cooking-ninja/src/pages/create/Create.js
+++ cooking-ninja/src/pages/create/Create.js
@@ -1,5 +1,5 @@
 import { useState, useRef, useEffect } from 'react'
-import { useFetch } from '../../hooks/useFetch'
+import { projectFirestore } from '../../firebase/config'
 import { useHistory } from 'react-router-dom'
 
 // styles
@@ -13,12 +13,18 @@ export default function Create() {
   const [ingredients, setIngredients] = useState([])
   const ingredientInput = useRef(null)
 
-  const { postData, data } = useFetch('http://localhost:3000/recipes', 'POST')
   const history = useHistory()
   
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault()
-    postData({ title, ingredients, method, cookingTime: cookingTime + ' minutes' })
+    const doc = { title, ingredients, method, cookingTime: cookingTime + ' minutes' }
+
+    try {
+      await projectFirestore.collection('recipes').add(doc)
+      history.push('/')
+    } catch (err) {
+      console.log(err)
+    }
   }
 
   const handleAdd = (e) => {
@@ -31,13 +37,6 @@ export default function Create() {
     setNewIngredient('')
     ingredientInput.current.focus()
   }
-
-  // redirect the user when we get data response
-  useEffect(() => {
-    if (data) {
-      history.push('/')
-    }
-  }, [data, history])
 
   return (
     <div className="create">
