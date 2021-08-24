e0e45964978ba887124f304171ee685cce76d013
Shaun <9838872+iamshaunjp@users.noreply.github.com>
2021-08-24 11:35:37 +0100

    Update useAuthContext.js

--- my-money/src/hooks/useAuthContext.js
+++ my-money/src/hooks/useAuthContext.js
@@ -5,7 +5,7 @@ export const useAuthContext = () => {
   const context = useContext(AuthContext)
 
   if(!context) {
-    throw Error('useAuthContext must be used outside of AuthContextProvider')
+    throw Error('useAuthContext must be used inside an AuthContextProvider')
   }
 
   return context
