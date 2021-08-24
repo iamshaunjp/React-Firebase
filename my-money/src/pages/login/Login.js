f5bb0d9cd5aa3726886f0a8ba55c5c48eda9c517
Shaun <shaun@thenetninja.co.uk>
2021-08-24 10:33:44 +0100

    lesson-115

--- my-money/src/pages/login/Login.js
+++ my-money/src/pages/login/Login.js
@@ -1,10 +1,37 @@
+import { useState } from 'react'
+
 // styles
 import styles from './Login.module.css'
 
 export default function Login() {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(email, password)
+  }
+
   return (
-    <div>
-      Login
-    </div>
+    <form onSubmit={handleSubmit} className={styles['login-form']}>
+      <h2>login</h2>
+      <label>
+        <span>email:</span>
+        <input 
+          type="email" 
+          onChange={(e) => setEmail(e.target.value)} 
+          value={email}
+        />
+      </label>
+      <label>
+        <span>password:</span>
+        <input 
+          type="password" 
+          onChange={(e) => setPassword(e.target.value)} 
+          value={password} 
+        />
+      </label>
+      <button className="btn">Login</button>
+    </form>
   )
 }
