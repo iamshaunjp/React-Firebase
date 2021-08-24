a3fba5b935b795c5efa1327e1f02aa7994777390
Shaun <shaun@thenetninja.co.uk>
2021-08-24 16:54:40 +0100

    lesson-139

--- my-money/src/hooks/useCollection.js
+++ my-money/src/hooks/useCollection.js
@@ -1,12 +1,20 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState, useRef } from "react"
 import { projectFirestore } from "../firebase/config"
 
-export const useCollection = (collection) => {
+export const useCollection = (collection, _query) => {
   const [documents, setDocuments] = useState(null)
   const [error, setError] = useState(null)
 
+  // if we don't use a ref --> infinite loop in useEffect
+  // _query is an array and is "different" on every function call
+  const query = useRef(_query).current
+
   useEffect(() => {
     let ref = projectFirestore.collection(collection)
+
+    if (query) {
+      ref = ref.where(...query)
+    }
 
     const unsubscribe = ref.onSnapshot(snapshot => {
       let results = []
@@ -26,7 +34,7 @@ export const useCollection = (collection) => {
     // unsubscribe on unmount
     return () => unsubscribe()
 
-  }, [collection])
+  }, [collection, query])
 
   return { documents, error }
 }
--- my-money/src/pages/home/Home.js
+++ my-money/src/pages/home/Home.js
@@ -10,7 +10,9 @@ import TransactionList from './TransactionList'
 
 export default function Home() {
   const { user } = useAuthContext()
-  const { documents, error } = useCollection('transactions')
+  const { documents, error } = useCollection(
+    'transactions', ["uid", "==", user.uid]
+  )
 
   return (
     <div className={styles.container}>
