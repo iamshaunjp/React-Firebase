dfb47fc96d2432741728d070e3bbaa367db13589
Shaun <shaun@thenetninja.co.uk>
2021-08-24 15:09:22 +0100

    lesson-129

--- my-money/src/context/AuthContext.js
+++ my-money/src/context/AuthContext.js
@@ -1,4 +1,5 @@
-import { createContext, useReducer } from 'react'
+import { createContext, useReducer, useEffect } from 'react'
+import { projectAuth } from '../firebase/config'
 
 export const AuthContext = createContext()
 
@@ -8,6 +9,8 @@ export const authReducer = (state, action) => {
       return { ...state, user: action.payload }
     case 'LOGOUT':
       return { ...state, user: null }
+    case 'AUTH_IS_READY':
+      return { user: action.payload, authIsReady: true }
     default:
       return state
   }
@@ -15,8 +18,17 @@ export const authReducer = (state, action) => {
 
 export const AuthContextProvider = ({ children }) => {
   const [state, dispatch] = useReducer(authReducer, { 
-    user: null 
+    user: null,
+    authIsReady: false
   })
+
+  useEffect(() => {
+    const unsub = projectAuth.onAuthStateChanged(user => {
+      dispatch({ type: 'AUTH_IS_READY', payload: user })
+      unsub()
+    })
+  }, [])
+
   console.log('AuthContext state:', state)
   
   return (
