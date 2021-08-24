fb90d51e425c8dedb26775f017ea53a91e5c8ba9
Shaun <shaun@thenetninja.co.uk>
2021-08-24 10:55:52 +0100

    lesson-118

--- my-money/src/firebase/config.js
+++ my-money/src/firebase/config.js
@@ -1,5 +1,6 @@
 import firebase from 'firebase/app'
 import 'firebase/firestore'
+import 'firebase/auth'
 
 const firebaseConfig = {
   apiKey: "AIzaSyDCeOytuIxNGchrGq4CrZq4cpohiIX9uYg",
@@ -16,5 +17,6 @@ firebase.initializeApp(firebaseConfig)
 
 // init services
 const projectFirestore = firebase.firestore()
+const projectAuth = firebase.auth()
 
-export { projectFirestore }
+export { projectFirestore, projectAuth }
