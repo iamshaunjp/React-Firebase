e516177f2df9c5b39c7e0c4af30733d0fe41d5fe
Shaun <shaun@thenetninja.co.uk>
2021-08-24 10:53:35 +0100

    lesson-117

---
+++ my-money/src/firebase/config.js
@@ -0,0 +1,20 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDCeOytuIxNGchrGq4CrZq4cpohiIX9uYg",
+  authDomain: "testing-123-c16fc.firebaseapp.com",
+  projectId: "testing-123-c16fc",
+  storageBucket: "testing-123-c16fc.appspot.com",
+  messagingSenderId: "934001709223",
+  appId: "1:934001709223:web:3a1ce8022d26e440b30b82",
+  measurementId: "G-3L5DFDF70R"
+}
+
+// init firebase
+firebase.initializeApp(firebaseConfig)
+
+// init services
+const projectFirestore = firebase.firestore()
+
+export { projectFirestore }
