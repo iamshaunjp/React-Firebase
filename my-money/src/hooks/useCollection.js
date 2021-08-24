ca3948d85cc429f43d574f7676188a811551c458
Shaun <shaun@thenetninja.co.uk>
2021-08-24 16:27:56 +0100

    lesson-137

---
+++ my-money/src/hooks/useCollection.js
@@ -0,0 +1,31 @@
+import { useEffect, useState } from "react"
+import { projectFirestore } from "../firebase/config"
+
+export const useCollection = (collection) => {
+  const [documents, setDocuments] = useState(null)
+  const [error, setError] = useState(null)
+
+  useEffect(() => {
+    let ref = projectFirestore.collection(collection)
+
+    const unsubscribe = ref.onSnapshot(snapshot => {
+      let results = []
+      snapshot.docs.forEach(doc => {
+        results.push({...doc.data(), id: doc.id})
+      });
+      
+      // update state
+      setDocuments(results)
+      setError(null)
+    }, error => {
+      console.log(error)
+      setError('could not fetch the data')
+    })
+
+    // unsubscribe on unmount
+    return () => unsubscribe()
+
+  }, [collection])
+
+  return { documents, error }
+}
