bc8ec5a9cd3d5d3c2719a1b6ce5e3be5813b6fdb
Shaun <shaun@thenetninja.co.uk>
2021-08-24 10:44:29 +0100

    lesson-116

--- my-money/src/pages/signup/Signup.js
+++ my-money/src/pages/signup/Signup.js
@@ -1,10 +1,46 @@
+import { useState } from 'react'
+
 // styles
 import styles from './Signup.module.css'
 
 export default function Signup() {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [displayName, setDisplayName] = useState('')
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(email, password, displayName)
+  }
+
   return (
-    <div>
-      Signup
-    </div>
+    <form onSubmit={handleSubmit} className={styles['signup-form']}>
+      <h2>sign up</h2>
+      <label>
+        <span>email:</span>
+        <input 
+          type="email" 
+          onChange={(e) => setEmail(e.target.value)} 
+          value={email}
+        />
+      </label>
+      <label>
+        <span>password:</span>
+        <input 
+          type="password" 
+          onChange={(e) => setPassword(e.target.value)} 
+          value={password} 
+        />
+      </label>
+      <label>
+        <span>display name:</span>
+        <input 
+          type="text" 
+          onChange={(e) => setDisplayName(e.target.value)}
+          value={displayName}
+        />
+      </label>
+      <button className="btn">Sign up</button>
+    </form>
   )
 }
