1acbeea2f5d770730ce296a7f076b9ce41b2571d
Shaun <shaun@thenetninja.co.uk>
2021-08-24 11:44:55 +0100

    lesson-124

--- my-money/src/components/Navbar.js
+++ my-money/src/components/Navbar.js
@@ -1,9 +1,12 @@
 import { Link } from "react-router-dom"
+import { useLogout } from '../hooks/useLogout'
 
 // styles
 import styles from './Navbar.module.css'
 
 export default function Navbar() {
+  const { logout } = useLogout()
+
   return (
     <nav className={styles.navbar}>
       <ul>
@@ -11,6 +14,10 @@ export default function Navbar() {
         
         <li><Link to="/login">Login</Link></li>
         <li><Link to="/signup">Signup</Link></li>
+
+        <li>
+          <button className="btn" onClick={logout}>Logout</button>
+        </li>
       </ul>
     </nav>
   )
