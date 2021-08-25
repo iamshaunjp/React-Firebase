3c2d476def6d83047ca7f0bc6c9485b17c8c7d31
Shaun <shaun@thenetninja.co.uk>
2021-08-25 12:27:30 +0100

    lesson-151

---
+++ the-dojo/src/firebase/config.js
@@ -0,0 +1,24 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/auth'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDXBeEYHuAA42LMCkxhdsOoYdMO2n7tgos",
+  authDomain: "udemy-files-dojo.firebaseapp.com",
+  projectId: "udemy-files-dojo",
+  storageBucket: "udemy-files-dojo.appspot.com",
+  messagingSenderId: "244633033606",
+  appId: "1:244633033606:web:aa1833bda9c7906227ea07"
+}
+
+// init firebase
+firebase.initializeApp(firebaseConfig)
+
+// init services
+const projectFirestore = firebase.firestore()
+const projectAuth = firebase.auth()
+
+// timestamp
+const timestamp = firebase.firestore.Timestamp
+
+export { projectFirestore, projectAuth, timestamp }
