83c4664119fb4c438f880e9e05d0f9dac20fdfec
Shaun <shaun@thenetninja.co.uk>
2021-08-25 10:30:15 +0100

    lesson-148

--- my-money/src/components/Navbar.js
+++ my-money/src/components/Navbar.js
@@ -12,7 +12,7 @@ export default function Navbar() {
   return (
     <nav className={styles.navbar}>
       <ul>
-        <li className={styles.title}>myMoney</li>
+        <li className={styles.title}>myMoneyApp</li>
         
         {!user && (
           <>
