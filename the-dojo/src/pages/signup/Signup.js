db9923395f4aae820717bb1cd5ca5ae52e84ae5c
Shaun <shaun@thenetninja.co.uk>
2021-08-25 15:31:48 +0100

    lesson-158

--- the-dojo/src/pages/signup/Signup.js
+++ the-dojo/src/pages/signup/Signup.js
@@ -8,10 +8,34 @@ export default function Signup() {
   const [password, setPassword] = useState('')
   const [displayName, setDisplayName] = useState('')
   const [thumbnail, setThumbnail] = useState(null)
+  const [thumbnailError, setThumbnailError] = useState(null)
   
   const handleSubmit = (e) => {
     e.preventDefault()
-    console.log(email, password, displayName)
+    console.log(email, password, displayName, thumbnail)
+  }
+
+  const handleFileChange = (e) => {
+    setThumbnail(null)
+    let selected = e.target.files[0]
+    console.log(selected)
+
+    if (!selected) {
+      setThumbnailError('Please select a file')
+      return
+    }
+    if (!selected.type.includes('image')) {
+      setThumbnailError('Selected file must be an image')
+      return
+    }
+    if (selected.size > 100000) {
+      setThumbnailError('Image file size must be less than 100kb')
+      return
+    }
+    
+    setThumbnailError(null)
+    setThumbnail(selected)
+    console.log('thumbnail updated')
   }
 
   return (
@@ -31,7 +55,8 @@ export default function Signup() {
       </label>
       <label>
         <span>Profile thumbnail:</span>
-        <input type="file" />
+        <input type="file" onChange={handleFileChange} />
+        {thumbnailError && <div className="error">{thumbnailError}</div>}
       </label>
       <button className="btn">Sign up</button>
     </form>
