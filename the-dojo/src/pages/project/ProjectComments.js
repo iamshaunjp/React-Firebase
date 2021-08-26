4357e4557493c8de3a7cce05c994c61bf1da02b0
Shaun <shaun@thenetninja.co.uk>
2021-08-26 19:09:08 +0100

    lesson-185

--- the-dojo/src/pages/project/ProjectComments.js
+++ the-dojo/src/pages/project/ProjectComments.js
@@ -2,6 +2,7 @@ import { useState } from "react"
 import { timestamp } from "../../firebase/config"
 import { useAuthContext } from "../../hooks/useAuthContext"
 import { useFirestore } from "../../hooks/useFirestore"
+import Avatar from "../../components/Avatar"
 
 export default function ProjectComments({ project }) {
   const { user } = useAuthContext()
@@ -30,6 +31,23 @@ export default function ProjectComments({ project }) {
   return (
     <div className="project-comments">
       <h4>Project Comments</h4>
+
+      <ul>
+        {project.comments.length > 0 && project.comments.map(comment => (
+          <li key={comment.id}>
+            <div className="comment-author">
+              <Avatar src={comment.photoURL} />
+              <p>{comment.displayName}</p>
+            </div>
+            <div className="comment-date">
+              <p>date here</p>
+            </div>
+            <div className="comment-content">
+              <p>{comment.content}</p>
+            </div>
+          </li>
+        ))}
+      </ul>
 
       <form className="add-comment" onSubmit={handleSubmit}>
         <label>
