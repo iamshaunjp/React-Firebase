8fe8bb3cb1ec6a26526d00abaa20b47839cb361e
Shaun <shaun@thenetninja.co.uk>
2021-08-26 18:43:34 +0100

    lesson-180

--- the-dojo/src/pages/project/Project.js
+++ the-dojo/src/pages/project/Project.js
@@ -1,10 +1,23 @@
+import { useParams } from "react-router-dom"
+import { useDocument } from '../../hooks/useDocument'
+
 // styles
 import './Project.css'
 
 export default function Project() {
+  const { id } = useParams()
+  const { document, error } = useDocument('projects', id)
+
+  if (error) {
+    return <div className="error">{error}</div>
+  }
+  if (!document) {
+    return <div className="loading">Loading...</div>
+  }
+
   return (
-    <div>
-      Project details
+    <div className="project-details">
+      <h1>{document.name}</h1>
     </div>
   )
 }
