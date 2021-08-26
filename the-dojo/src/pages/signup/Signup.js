65e069adbbaf09e7aa145d7cf3fbd2f2b187332f
Shaun <shaun@thenetninja.co.uk>
2021-08-26 13:52:10 +0100

    lesson-157

--- the-dojo/src/pages/signup/Signup.js
+++ the-dojo/src/pages/signup/Signup.js
@@ -19,19 +19,37 @@ export default function Signup() {
       <h2>sign up</h2>
       <label>
         <span>email:</span>
-        <input type="email" onChange={(e) => setEmail(e.target.value)} />
+        <input
+          required 
+          type="email" 
+          onChange={(e) => setEmail(e.target.value)} 
+          value={email}
+        />
       </label>
       <label>
         <span>password:</span>
-        <input type="password" onChange={(e) => setPassword(e.target.value)} />
+        <input
+          required
+          type="password" 
+          onChange={(e) => setPassword(e.target.value)} 
+          value={password}
+        />
       </label>
       <label>
         <span>display name:</span>
-        <input type="text" onChange={(e) => setDisplayName(e.target.value)} />
+        <input
+          required
+          type="text" 
+          onChange={(e) => setDisplayName(e.target.value)} 
+          value={displayName}
+        />
       </label>
       <label>
         <span>Profile thumbnail:</span>
-        <input type="file" />
+        <input 
+          required
+          type="file" 
+        />
       </label>
       <button className="btn">Sign up</button>
     </form>
