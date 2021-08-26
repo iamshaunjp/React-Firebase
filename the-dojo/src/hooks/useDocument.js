e3d96fc02eade6a13e158c9379bde30fdd5d9047
Shaun <shaun@thenetninja.co.uk>
2021-08-26 18:39:51 +0100

    lesson-179

---
+++ the-dojo/src/hooks/useDocument.js
@@ -0,0 +1,32 @@
+import { useEffect, useState } from "react"
+import { projectFirestore } from "../firebase/config"
+
+export const useDocument = (collection, id) => {
+  const [document, setDocument] = useState(null)
+  const [error, setError] = useState(null)
+
+  // realtime document data
+  useEffect(() => {
+    const ref = projectFirestore.collection(collection).doc(id)
+
+    const unsubscribe = ref.onSnapshot(snapshot => {
+      // need to make sure the doc exists & has data
+      if(snapshot.data()) {
+        setDocument({...snapshot.data(), id: snapshot.id})
+        setError(null)
+      }
+      else {
+        setError('No such document exists')
+      }
+    }, err => {
+      console.log(err.message)
+      setError('failed to get document')
+    })
+
+    // unsubscribe on unmount
+    return () => unsubscribe()
+
+  }, [collection, id])
+
+  return { document, error }
+}
