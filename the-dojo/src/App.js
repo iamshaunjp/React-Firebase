32dfcc838cc538fe6ea0fc5fbe779401023bd44d
Shaun <shaun@thenetninja.co.uk>
2021-08-26 14:57:50 +0100

    lesson-169

--- the-dojo/src/App.js
+++ the-dojo/src/App.js
@@ -12,6 +12,7 @@ import Signup from './pages/signup/Signup'
 import Project from './pages/project/Project'
 import Navbar from './components/Navbar'
 import Sidebar from './components/Sidebar'
+import OnlineUsers from './components/OnlineUsers'
 
 function App() {
   const { authIsReady, user } = useAuthContext()
@@ -46,6 +47,7 @@ function App() {
               </Route>
             </Switch>
           </div>
+          <OnlineUsers />
         </BrowserRouter>
       )}
     </div>
---
+++ the-dojo/src/components/OnlineUsers.js
@@ -0,0 +1,25 @@
+import { useCollection } from '../hooks/useCollection'
+
+// components
+import Avatar from './Avatar'
+
+// styles
+import './OnlineUsers.css'
+
+export default function OnlineUsers() {
+  const { isPending, error, documents } = useCollection('users')
+
+  return (
+    <div className="user-list">
+      <h2>All Users</h2>
+      {isPending && <div>Loading users...</div>}
+      {error && <div>{error}</div>}
+      {documents && documents.map(user => (
+        <div key={user.id} className="user-list-item">
+          <span>{user.displayName}</span>
+          <Avatar src={user.photoURL} />
+        </div>
+      ))}
+    </div>
+  )
+}
