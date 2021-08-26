4456bea2cd1b46a116ee3d2ae6dd107e693c664b
Shaun <shaun@thenetninja.co.uk>
2021-08-26 18:34:49 +0100

    lesson-178

--- the-dojo/src/components/ProjectList.js
+++ the-dojo/src/components/ProjectList.js
@@ -1,12 +1,31 @@
+import { Link } from 'react-router-dom'
+import Avatar from '../components/Avatar'
+
 // styles
 import './ProjectList.css'
 
 export default function ProjectList({ projects }) {
   console.log(projects)
-  
+
   return (
-    <div>
-      project list
+    <div className="project-list">
+      {projects.length === 0 && <p>No projects yet!</p>}
+      {projects.map(project => (
+        <Link to={`/projects/${project.id}`} key={project.id}>
+          <h4>{project.name}</h4>
+          <p>Due by {project.dueDate.toDate().toDateString()}</p>
+          <div className="assigned-to">
+            <p><strong>Assigned to:</strong></p>
+            <ul>
+              {project.assignedUsersList.map(user => (
+                <li key={user.photoURL}>
+                  <Avatar src={user.photoURL} />
+                </li>
+              ))}
+            </ul>
+          </div>
+        </Link>
+      ))}
     </div>
   )
 }
