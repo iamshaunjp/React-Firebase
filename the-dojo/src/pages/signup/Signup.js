99f3311b5c709bb18b230fabace37beb723e3d0b
Shaun <shaun@thenetninja.co.uk>
2021-08-26 13:56:38 +0100

    lesson-162

--- the-dojo/src/pages/signup/Signup.js
+++ the-dojo/src/pages/signup/Signup.js
@@ -45,19 +45,38 @@ export default function Signup() {
       <h2>sign up</h2>
       <label>
         <span>email:</span>
-        <input type="email" onChange={(e) => setEmail(e.target.value)} />
+        <input
+          required 
+          type="email" 
+          onChange={(e) => setEmail(e.target.value)} 
+          value={email}
+        />
       </label>
       <label>
         <span>password:</span>
-        <input type="password" onChange={(e) => setPassword(e.target.value)} />
+        <input
+          required
+          type="password" 
+          onChange={(e) => setPassword(e.target.value)} 
+          value={password}
+        />
       </label>
       <label>
         <span>display name:</span>
-        <input type="text" onChange={(e) => setDisplayName(e.target.value)} />
+        <input
+          required
+          type="text" 
+          onChange={(e) => setDisplayName(e.target.value)} 
+          value={displayName}
+        />
       </label>
       <label>
         <span>Profile thumbnail:</span>
-        <input type="file" onChange={handleFileChange} />
+        <input 
+          required
+          type="file"
+          onChange={handleFileChange}
+        />
         {thumbnailError && <div className="error">{thumbnailError}</div>}
       </label>
       {!isPending && <button className="btn">Sign up</button>}
