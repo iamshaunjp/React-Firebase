339310228054606c7504cf3b3c34fb1e6917ef78
Shaun <shaun@thenetninja.co.uk>
2021-08-26 14:27:24 +0100

    lesson-164

--- the-dojo/src/pages/login/Login.js
+++ the-dojo/src/pages/login/Login.js
@@ -1,10 +1,39 @@
+import { useState } from 'react'
+
 // styles
 import './Login.css'
 
 export default function Login() {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(email, password)
+  }
+
   return (
-    <div>
-      Login
-    </div>
+    <form onSubmit={handleSubmit} className="auth-form">
+      <h2>login</h2>
+      <label>
+        <span>email:</span>
+        <input
+          required
+          type="email" 
+          onChange={(e) => setEmail(e.target.value)} 
+          value={email} 
+        />
+      </label>
+      <label>
+        <span>password:</span>
+        <input 
+          required
+          type="password" 
+          onChange={(e) => setPassword(e.target.value)} 
+          value={password} 
+        />
+      </label>
+      <button className="btn">Log in</button>
+    </form>
   )
 }
