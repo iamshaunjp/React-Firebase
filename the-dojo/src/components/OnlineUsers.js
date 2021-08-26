99e4fd10ff674885d10919d34c1a61969cbdc26e
Shaun <shaun@thenetninja.co.uk>
2021-08-26 15:00:23 +0100

    lesson-170

--- the-dojo/src/components/OnlineUsers.js
+++ the-dojo/src/components/OnlineUsers.js
@@ -16,6 +16,7 @@ export default function OnlineUsers() {
       {error && <div>{error}</div>}
       {documents && documents.map(user => (
         <div key={user.id} className="user-list-item">
+          {user.online && <span className="online-user"></span>}
           <span>{user.displayName}</span>
           <Avatar src={user.photoURL} />
         </div>
