155fb193633b1eba1d3dee69fc62f7501c251764
Shaun <shaun@thenetninja.co.uk>
2021-08-26 19:20:21 +0100

    lesson-187

--- the-dojo/src/pages/dashboard/Dashboard.js
+++ the-dojo/src/pages/dashboard/Dashboard.js
@@ -5,6 +5,7 @@ import ProjectList from '../../components/ProjectList'
 
 // styles
 import './Dashboard.css'
+import ProjectFilter from './ProjectFilter'
 
 export default function Dashboard() {
   const { documents, error } = useCollection('projects')
@@ -13,6 +14,7 @@ export default function Dashboard() {
     <div>
       <h2 className="page-title">Dashboard</h2>
       {error && <p className="error">{error}</p>}
+      {documents && <ProjectFilter />}
       {documents && <ProjectList projects={documents} />}
     </div>
   )
---
+++ the-dojo/src/pages/dashboard/ProjectFilter.js
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+
+const filterList = ['all', 'mine', 'development', 'design', 'marketing', 'sales']
+
+export default function ProjectFilter() {
+  const [currentFilter, setCurrentFilter] = useState('all')
+
+  const handleClick = (newFilter) => {
+    setCurrentFilter(newFilter)
+  }
+
+  return (
+    <div className="project-filter">
+      <nav>
+        <p>Filter by: </p>
+        {filterList.map((f) => (
+          <button key={f}
+            onClick={() => handleClick(f)}
+            className={currentFilter === f ? 'active' : ''}
+          >{f}</button>
+        ))}
+      </nav>
+    </div>
+  )
+}
