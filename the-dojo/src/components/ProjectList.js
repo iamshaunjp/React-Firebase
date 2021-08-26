46f802d1fac4d5782ce9f2730a7f1c0e7f289a8e
Shaun <shaun@thenetninja.co.uk>
2021-08-26 18:26:58 +0100

    lesson-177

---
+++ the-dojo/src/components/ProjectList.js
@@ -0,0 +1,12 @@
+// styles
+import './ProjectList.css'
+
+export default function ProjectList({ projects }) {
+  console.log(projects)
+  
+  return (
+    <div>
+      project list
+    </div>
+  )
+}
--- the-dojo/src/pages/dashboard/Dashboard.js
+++ the-dojo/src/pages/dashboard/Dashboard.js
@@ -1,10 +1,19 @@
+import { useCollection } from '../../hooks/useCollection'
+
+// components
+import ProjectList from '../../components/ProjectList'
+
 // styles
 import './Dashboard.css'
 
-export default function dashboard() {
+export default function Dashboard() {
+  const { documents, error } = useCollection('projects')
+
   return (
     <div>
-      Dashboard
+      <h2 className="page-title">Dashboard</h2>
+      {error && <p className="error">{error}</p>}
+      {documents && <ProjectList projects={documents} />}
     </div>
   )
 }
