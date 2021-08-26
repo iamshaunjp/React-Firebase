0c00b8dd96b89b4403875108de4018ece6d58cf9
Shaun <shaun@thenetninja.co.uk>
2021-08-26 20:59:01 +0100

    lesson-191

--- the-dojo/src/pages/project/ProjectComments.js
+++ the-dojo/src/pages/project/ProjectComments.js
@@ -3,6 +3,7 @@ import { timestamp } from "../../firebase/config"
 import { useAuthContext } from "../../hooks/useAuthContext"
 import { useFirestore } from "../../hooks/useFirestore"
 import Avatar from "../../components/Avatar"
+import formatDistanceToNow from 'date-fns/formatDistanceToNow'
 
 export default function ProjectComments({ project }) {
   const { user } = useAuthContext()
@@ -40,7 +41,7 @@ export default function ProjectComments({ project }) {
               <p>{comment.displayName}</p>
             </div>
             <div className="comment-date">
-              <p>date here</p>
+              <p>{formatDistanceToNow(comment.createdAt.toDate(), {addSuffix: true})}</p>
             </div>
             <div className="comment-content">
               <p>{comment.content}</p>
