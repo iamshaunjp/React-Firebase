89d566dbf43941cd5e73f82226423da156252dcc
Shaun <9838872+iamshaunjp@users.noreply.github.com>
2021-08-27 10:36:45 +0100

    Update ProjectList.js

--- the-dojo/src/components/ProjectList.js
+++ the-dojo/src/components/ProjectList.js
@@ -6,6 +6,7 @@ export default function ProjectList({ projects }) {
   
   return (
     <div>
+      {projects.length === 0 && <p>No projects yet!</p>}
       {projects.map(project => (
         <div key={project.id}>{project.title}</div> 
       ))}
