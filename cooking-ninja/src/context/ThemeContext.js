a16708cbe9f1530053da1bd063556719667e6e81
squagalings <justineyongjzxy@gmail.com>
2022-01-18 20:52:54 -0800

    g

---
+++ cooking-ninja/src/context/ThemeContext.js
@@ -0,0 +1,11 @@
+import {creatContext, createContext} from 'react'
+
+export const ThemeContext = createContext()
+
+export function ThemeProvider({children}) {
+  return (
+    <ThemeContext.Provider value={{color: 'blue'}}>
+      {children}
+    </ThemeContext.Provider>
+  )
+}
