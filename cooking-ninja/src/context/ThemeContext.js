ed0870669c30eaa413a20003ab9b1d04551dcb36
squagalings <justineyongjzxy@gmail.com>
2022-01-21 15:27:04 -0800

    ;

--- cooking-ninja/src/context/ThemeContext.js
+++ cooking-ninja/src/context/ThemeContext.js
@@ -3,6 +3,8 @@ import {creatContext, createContext} from 'react'
 export const ThemeContext = createContext()
 
 export function ThemeProvider({children}) {
+
+  // custon logic
   
   return (
     <ThemeContext.Provider value={{color: 'blue'}}>
