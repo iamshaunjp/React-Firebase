2d383f5e5efd2a12d4fc611fadec1a80cea8440b
jamiemn86 <jamie.a.mollernielsen@gmail.com>
2022-06-25 19:58:43 +0200

    Solution to challenge in lesson 63

--- magic-memory/src/App.js
+++ magic-memory/src/App.js
@@ -1,31 +1,56 @@
-import { useState } from 'react'
-import './App.css'
-import SingleCard from './components/SingleCard'
+import { useState, useEffect } from 'react';
+import './App.css';
+import SingleCard from './components/SingleCard';
 
 const cardImages = [
-  { "src": "/img/helmet-1.png" },
-  { "src": "/img/potion-1.png" },
-  { "src": "/img/ring-1.png" },
-  { "src": "/img/scroll-1.png" },
-  { "src": "/img/shield-1.png" },
-  { "src": "/img/sword-1.png" },
-]
+  { src: '/img/helmet-1.png' },
+  { src: '/img/potion-1.png' },
+  { src: '/img/ring-1.png' },
+  { src: '/img/scroll-1.png' },
+  { src: '/img/shield-1.png' },
+  { src: '/img/sword-1.png' }
+];
 
 function App() {
-  const [cards, setCards] = useState([])
-  const [turns, setTurns] = useState(0)
+  const [cards, setCards] = useState([]);
+  const [turns, setTurns] = useState(0);
+  const [choiceOne, setChoiceOne] = useState(null);
+  const [choiceTwo, setChoiceTwo] = useState(null);
 
   // shuffle cards for new game
   const shuffleCards = () => {
     const shuffledCards = [...cardImages, ...cardImages]
       .sort(() => Math.random() - 0.5)
-      .map(card => ({ ...card, id: Math.random() }))
-      
-    setCards(shuffledCards)
-    setTurns(0)
-  }
+      .map((card) => ({ ...card, id: Math.random() }));
 
-  console.log(cards, turns)
+    setCards(shuffledCards);
+    setTurns(0);
+  };
+
+  // handle a choice
+  const handleChoice = (card) => {
+    // console.log(card);
+    choiceOne ? setChoiceTwo(card) : setChoiceOne(card);
+  };
+
+  // compare cards and log to console
+  useEffect(() => {
+    if (choiceTwo != null) {
+      console.log(choiceTwo.src);
+      if (choiceTwo.src === choiceOne.src && choiceTwo.id !== choiceOne.id) {
+        console.log('The two same cards were selected');
+      }
+    }
+    resetTurn();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [choiceTwo]);
+
+  // reset choices & increase turn
+  const resetTurn = () => {
+    setChoiceOne(null);
+    setChoiceTwo(null);
+    setTurns((prevTurns) => prevTurns + 1);
+  };
 
   return (
     <div className="App">
@@ -33,16 +58,12 @@ function App() {
       <button onClick={shuffleCards}>New Game</button>
 
       <div className="card-grid">
-        {cards.map(card => (
-          <SingleCard 
-            key={card.id}
-            card={card}
-          />
+        {cards.map((card) => (
+          <SingleCard key={card.id} card={card} handleChoice={handleChoice} />
         ))}
       </div>
-
     </div>
   );
 }
 
-export default App
+export default App;
--- magic-memory/src/components/SingleCard.js
+++ magic-memory/src/components/SingleCard.js
@@ -1,12 +1,21 @@
-import './SingleCard.css'
+import './SingleCard.css';
 
-export default function SingleCard({ card }) {
+export default function SingleCard({ card, handleChoice }) {
+  const handleClick = () => {
+    handleChoice(card);
+  };
+
   return (
     <div className="card">
       <div>
         <img className="front" src={card.src} alt="card front" />
-        <img className="back" src="/img/cover.png" alt="cover" />
+        <img
+          className="back"
+          src="/img/cover.png"
+          onClick={handleClick}
+          alt="cover"
+        />
       </div>
     </div>
-  )
+  );
 }
