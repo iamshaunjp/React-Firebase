5818f5156ed3604d875bc12704ad0135c6598048
cameronroberts00 <cameronroberts00@gmail.com>
2022-07-13 10:58:14 +0100

    did challenge
    did challenge

--- my-first-react-app/src/App.js
+++ my-first-react-app/src/App.js
@@ -1,33 +1,36 @@
-import './App.css'
-import React, { useState } from 'react'
-import Title from './components/Title'
-import Modal from './components/Modal'
+import "./App.css";
+import React, { useState } from "react";
+import Title from "./components/Title";
+import Modal from "./components/Modal";
 
 function App() {
-  const [showModal, setShowModal] = useState(true)
-  const [showEvents, setShowEvents] = useState(true)
+  const [showModal, setShowModal] = useState(false);
+  const [showEvents, setShowEvents] = useState(true);
   const [events, setEvents] = useState([
-    {title: "mario's birthday bash", id: 1},
-    {title: "bowser's live stream", id: 2},
-    {title: "race on moo moo farm", id: 3}
-  ])
+    { title: "mario's birthday bash", id: 1 },
+    { title: "bowser's live stream", id: 2 },
+    { title: "race on moo moo farm", id: 3 },
+  ]);
 
   const handleClick = (id) => {
-    setEvents(prevEvents => {
-      return prevEvents.filter(event => id !== event.id)
-    })
-  }
+    setEvents((prevEvents) => {
+      return prevEvents.filter((event) => id !== event.id);
+    });
+  };
 
   const handleClose = () => {
-    setShowModal(false)
-  }
+    setShowModal(false);
+  };
 
-  const subtitle = "All the latest events in Marioland"
+  const subtitle = "All the latest events in Marioland";
 
   return (
     <div className="App">
+       <button onClick={()=>setShowModal(true)}>Trigger Modal</button>
+ <br />
       <Title title="Marioland Events" subtitle={subtitle} />
-      
+ 
+     
       {showEvents && (
         <div>
           <button onClick={() => setShowEvents(false)}>Hide Events</button>
@@ -38,17 +41,25 @@ function App() {
           <button onClick={() => setShowEvents(true)}>Show Events</button>
         </div>
       )}
-      {showEvents && events.map((event, index) => (
-        <React.Fragment key={event.id}>
-          <h2>{index} - {event.title}</h2>
-          <button onClick={() => handleClick(event.id)}>delete event</button>
-        </React.Fragment>
-      ))}
+      {showEvents &&
+        events.map((event, index) => (
+          <React.Fragment key={event.id}>
+            <h2>
+              {index} - {event.title}
+            </h2>
+            <button onClick={() => handleClick(event.id)}>delete event</button>
+          </React.Fragment>
+        ))}
 
       {showModal && (
         <Modal handleClose={handleClose}>
           <h2>Terms and Conditions</h2>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error odit nam et reprehenderit quibusdam temporibus officia dolorum quo sint nemo quis, laborum, quasi nisi fugit praesentium debitis repudiandae! Sapiente, omnis.</p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error odit
+            nam et reprehenderit quibusdam temporibus officia dolorum quo sint
+            nemo quis, laborum, quasi nisi fugit praesentium debitis
+            repudiandae! Sapiente, omnis.
+          </p>
         </Modal>
       )}
     </div>
