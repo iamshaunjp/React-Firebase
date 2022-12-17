df9b74ea065193b103109513f178d9fd27e0cdc6
Zoltan Kepes <74784960+Zolske@users.noreply.github.com>
2022-12-17 10:52:54 +0000

    missing "{}" to list ingredients
    There is a small typo, "{ }" are missing around the "ing" on line 21 in the <li> tag.
    
    Thank you for the amazing content :) , you are my hero.

--- cooking-ninja/src/pages/recipe/Recipe.js
+++ cooking-ninja/src/pages/recipe/Recipe.js
@@ -18,7 +18,7 @@ export default function Recipe() {
           <h2 className="page-title">{recipe.title}</h2>
           <p>Takes {recipe.cookingTime} to cook.</p>
           <ul>
-            {recipe.ingredients.map(ing => <li key={ing}>ing</li>)}
+            {recipe.ingredients.map(ing => <li key={ing}>{ing}</li>)}
           </ul>
           <p className="method">{recipe.method}</p>
         </>
